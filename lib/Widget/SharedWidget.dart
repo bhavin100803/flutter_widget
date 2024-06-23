cfe1bcf91eab2e8f8daae9a2b80d5b49d73d8cd5
Bhavin Makwana <bhavinmakwana293@example.com>
2024-06-23 10:23:17 +0530

    update shared

--- lib/Widget/SharedWidget.dart
+++ lib/Widget/SharedWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Sharedwidget extends StatefulWidget {
   const Sharedwidget({super.key});
@@ -10,6 +11,17 @@ class Sharedwidget extends StatefulWidget {
 
 class _SharedwidgetState extends State<Sharedwidget> {
   var nameController = TextEditingController();
+
+  static const String KEYNAME = "name";
+
+  var nameValue = "No Value Saved";
+
+  @override
+  void initState() {
+    super.initState();
+    getValue();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,32 +29,52 @@ class _SharedwidgetState extends State<Sharedwidget> {
           title: Text("Shared Pref"),
           backgroundColor: Colors.blue,
         ),
-        body:Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: nameController,
-                decoration: InputDecoration(
-                    label: Text('Name'),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(21)
-                    )
+        body:Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                      label: Text('Name'),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(21)
+                      )
+                  ),
                 ),
-              ),
-              SizedBox(height: 11,),
-              ElevatedButton(onPressed: () async {
-                   var name = nameController.text.toString();
-                  // var prefs = await SharedPreferences.getInstance();
-                  // prefs.setString("name",nameController.text.toString());
-              }, child: Text('Save')),
-              SizedBox(height: 11,),
-              Text('Saved Value')
+                SizedBox(height: 11,),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue
+                  ),
+                    onPressed: () async {
+                     var name = nameController.text.toString();
+                     var prefs = await SharedPreferences.getInstance();
+                     prefs.setString(KEYNAME,nameController.text.toString());
+                }, child: Text('Save',style: TextStyle(color: Colors.white),)),
+                SizedBox(height: 11,),
+                Text(nameValue)
 
-            ],
+              ],
+            ),
           ),
         )
     );
+  }
+
+  void getValue() async {
+
+      var prefs = await SharedPreferences.getInstance();
+
+      var getName =   prefs.getString(KEYNAME);
+
+      nameValue = getName ?? "No Value Saved";
+      setState(() {
+
+      });
+
   }
 }
 
